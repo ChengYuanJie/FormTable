78ad9b68632d96069b0152e6f183ec53de8c2555
ChengYuanJie <741699692@qq.com>
2019-11-22 18:03:00 +0800

    FormTable 1.0.0

---
+++ SourceFiles/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AVFoundation
+//
+//  Created by Aaron on 2019/10/17.
+//  Copyright © 2019 Aaron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
